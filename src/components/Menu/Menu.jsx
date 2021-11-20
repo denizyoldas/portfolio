59848c21db8c13f0f2accae71504d09c411ee82c
deniz.aksu <deniz.aksu@dijitalkurye.com.tr>
2021-11-21 00:04:44 +0300

    contact add

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -17,7 +17,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#testimonials">Testimonials</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#Contact">Contact</a>
+          <a href="#contact">Contact</a>
         </li>
       </ul>
     </div>
